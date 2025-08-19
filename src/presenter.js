d1e0c1846df1e7b909c216781da4c5c3c792e122
David-ZemaP <david@David.>
2025-08-18 22:37:11 -0400

    Añadir la funcion de multiplicar en la pagina

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,26 @@
-import sumar from "./sumador";
+import sumar from './sumador';
+import multiplicar from './multiplicador';
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const first = document.querySelector('#primer-numero');
+const second = document.querySelector('#segundo-numero');
+const form = document.querySelector('#sumar-form');
+const div = document.querySelector('#resultado-div');
+const mult = document.querySelector('#mult-button');
 
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = '<p>' + sumar(firstNumber, secondNumber) + '</p>';
+});
+
+mult.addEventListener('click', (event)=>{
+  event.preventDefault();
+
+  const firstn = Number.parseInt(first.value);
+  const secondn = Number.parseInt(second.value);
+
+  div.innerHTML = '<p>' + multiplicar(firstn, secondn) + '</p>';
 });
