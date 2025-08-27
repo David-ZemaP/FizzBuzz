88727e59d9be381c910b9075ef9461f77e32132b
David-ZemaP <david@David.>
2025-08-26 23:08:19 -0400

    feat: Green test for return 'Buzz' when the number is multiple of 5

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,11 +2,15 @@ function IsMultipleof3(n) {
   return n % 3 === 0;
 }
 
+function IsMultipleof5(n) {
+  return n % 5 === 0;
+}
+
 export function ConvertFizzBuzz(n) {
   if (IsMultipleof3(n)) {
     return 'Fizz';
   }
-  if (n === 5) {
+  if (IsMultipleof5(n)) {
     return 'Buzz';
   }
   return `${n}`;
