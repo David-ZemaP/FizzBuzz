2f4a450cf5f8971cacfb654ccd02e5d5ee89f0da
David-ZemaP <david@David.>
2025-08-27 00:29:14 -0400

    fix: Imports and erase files the project doesn't need

--- src/calculadora.js
+++
@@ -1,7 +0,0 @@
-export function sumador(a, b) {
-  return a + b;
-}
-
-export function multiplicador(a, b) {
-  return a * b;
-}
--- src/presenter.js
+++ src/presenter.js
@@ -1,25 +1,13 @@
-import { sumador, multiplicador } from './calculadora.js';
+import { generateFizzBuzzString } from './fizzbuzz.js';
 
-const first = document.querySelector('#primer-numero');
-const second = document.querySelector('#segundo-numero');
-const form = document.querySelector('#sumar-form');
+const n = document.querySelector('#numero');
 const div = document.querySelector('#resultado-div');
-const mult = document.querySelector('#mult-button');
+const form = document.querySelector('#fizzbuzz-form');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const number = Number.parseInt(n.value);
 
-  div.innerHTML = `<p> ${sumador(firstNumber, secondNumber)} </p>`;
-});
-
-mult.addEventListener('click', (event) => {
-  event.preventDefault();
-
-  const firstn = Number.parseInt(first.value);
-  const secondn = Number.parseInt(second.value);
-
-  div.innerHTML = `<p> ${multiplicador(firstn, secondn)}  </p>`;
+  div.innerHTML = `<p> ${generateFizzBuzzString(number)} </p>`;
 });
