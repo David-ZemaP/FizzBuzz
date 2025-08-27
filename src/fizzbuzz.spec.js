5472e8a40245e748e186e79722d94a459194f9aa
David-ZemaP <david@David.>
2025-08-26 22:53:04 -0400

    feat: Red test for convert any number into a string

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -1,7 +1,11 @@
 import { ConvertFizzBuzz } from './fizzbuzz';
 
 describe('FizzBuzz example', () => {
-  it('deberia retornar el mismo numero si no es multiplo de 3 o 5', () => {
+  it('deberia retornar 1', () => {
     expect(ConvertFizzBuzz(1)).toEqual('1');
+  });
+
+  it('deberia retorna cualquier numero si no es multiplo de 3 o 5', () => {
+    expect(ConvertFizzBuzz(7)).toEqual('7');
   });
 });
